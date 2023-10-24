749c8e9f6379eec1d7534c0076468707f8c52e21
gustavonjr <gustavonascimento_31@hotmail.com>
2023-10-23 21:15:36 -0300

    Atualização dos dados do usuario.

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -43,6 +47,65 @@ public class UserController {
             userService.delete(userModelOptional.get());
             return ResponseEntity.status(HttpStatus.OK).body("Usuario deletado com sucesso.");
         }
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<Object> updateUser(@PathVariable(value = "userId")UUID userId, @RequestBody @JsonView(UserDto.userView.UserPut.class) UserDto userDto){
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado.");
+        } else {
+            userService.delete(userModelOptional.get());
+            var userModel = userModelOptional.get();
+            userModel.setFullName(userDto.getFullName());
+            userModel.setCpf(userDto.getCpf());
+            userModel.setPhoneNumber(userDto.getPhoneNumber());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+            userService.save(userModel);
+
+            return ResponseEntity.status(HttpStatus.OK).body("Usuario atualizado com sucesso.");
+        }
+
+
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<Object> updateUserPassword(@PathVariable(value = "userId")UUID userId, @RequestBody @JsonView(UserDto.userView.PasswordPut.class) UserDto userDto){
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado.");
+        }  if (!userModelOptional.get().getPassword().equals(userDto.getOldPassword())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Senhas invalidas.");
+        }  else {
+            var userModel = userModelOptional.get();
+            userModel.setPassword(userDto.getPassword());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+            userService.save(userModel);
+
+            return ResponseEntity.status(HttpStatus.OK).body("Senha atualizada com sucesso!");
+        }
+
+
+    }
+
+    @PutMapping("/{userId}/image")
+    public ResponseEntity<Object> updateUserImage(@PathVariable(value = "userId")UUID userId, @RequestBody @JsonView(UserDto.userView.ImagePut.class) UserDto userDto){
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado.");
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setImageUrl(userDto.getImageUrl());
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+
+            userService.save(userModel);
+
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
+        }
+
+
     }
 
 }
--- src/main/java/com/ead/authuser/dtos/UserDto.java
+++ src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import java.util.UUID;
 
@@ -8,15 +9,32 @@ import java.util.UUID;
 @JsonInclude(JsonInclude.Include.NON_NULL) // Anotação para não levar para o endpoint campos que forem nulos
 public class UserDto {
 
+    public interface userView{
+        public static interface RegistrationPost{}
+        public static interface UserPut{}
+        public static interface PasswordPut{}
+        public static interface ImagePut{}
+    }
 
     private UUID userId;
+    @JsonView(userView.RegistrationPost.class)
     private String username;
+    @JsonView(userView.RegistrationPost.class)
     private String email;
+
+    @JsonView({userView.RegistrationPost.class, userView.PasswordPut.class})
     private String password;
+    @JsonView(userView.PasswordPut.class)
     private String oldPassword;
+
+    @JsonView({userView.RegistrationPost.class, userView.UserPut.class})
     private String fullName;
+    @JsonView({userView.RegistrationPost.class, userView.UserPut.class})
     private String phoneNumber;
+    @JsonView({userView.RegistrationPost.class, userView.UserPut.class})
     private String cpf;
+
+    @JsonView(userView.ImagePut.class)
     private String imageUrl;
 
 }
